915d888d9abeac86cec49b478c3a1bf53a04cc32
Isak <aslundisak@gmail.com>
2015-11-08 21:45:46 +0100

    Mark todo list item as complete

--- spec/features/todo_items/complete_spec.rb
+++ spec/features/todo_items/complete_spec.rb
@@ -5,9 +5,14 @@ describe "Adding todo items" do
 	let!(:todo_item) { todo_list.todo_items.create(content: "Milk") } 
 
 	it "is successfull when marking a single item complete" do
-		expect(todo_item.complete_at).to be_nil
-		visit_todo_list todo_list
-		within dom_id
+		expect(todo_item.completed_at).to be_nil
+		#parenteser
+		visit_todo_list(todo_list)
+		within dom_id_for(todo_item) do
+			click_link "Mark Complete"
+		end
+		todo_item.reload
+		expect(todo_item.completed_at).to_not be_nil
 
 
 	end
